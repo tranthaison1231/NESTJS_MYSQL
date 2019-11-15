31e6a8c9b97f4a0292240766a5d3799c5b4e501b
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-11-15 17:09:26 +0700

    Update

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(), UsersModule],
+})
+export class AppModule {}
